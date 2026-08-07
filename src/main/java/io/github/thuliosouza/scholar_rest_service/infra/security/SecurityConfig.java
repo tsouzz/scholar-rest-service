7f4ab37f718a5bd126774047f4a501005318cb20
thulio <thulioalves927@gmail.com>
2026-08-06 21:12:20 -0300

    feat(classgroup): Implementa operações de CRUD para professores

--- src/main/java/io/github/thuliosouza/scholar_rest_service/infra/security/SecurityConfig.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/infra/security/SecurityConfig.java
@@ -1,4 +1,15 @@
 package io.github.thuliosouza.scholar_rest_service.infra.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
