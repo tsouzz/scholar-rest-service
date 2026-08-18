30cc5a559a3d7c0a7dc8b8793edd8a532814a7ad
thulio <thulioalves927@gmail.com>
2026-08-17 22:24:39 -0300

    feat: implementa UserDetails na entidade Teacher

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/Teacher.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/Teacher.java
@@ -3,7 +3,14 @@ package io.github.thuliosouza.scholar_rest_service.domain.teacher;
 import io.github.thuliosouza.scholar_rest_service.domain.school.School;
 import jakarta.persistence.*;
 import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -13,7 +20,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Teacher {
+public class Teacher implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -32,4 +39,18 @@ public class Teacher {
     @JoinColumn(name = "school_id", nullable = false)
     private School school;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_TEACHER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
