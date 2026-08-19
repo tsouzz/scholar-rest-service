828f39ffebb78f326e5a9eb3c2938b9f4ea8dca4
thulio <thulioalves927@gmail.com>
2026-08-18 22:02:30 -0300

    feat: add TokenService for JWT generation and validation

--- src/main/java/io/github/thuliosouza/scholar_rest_service/infra/security/TokenService.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/infra/security/TokenService.java
@@ -1,4 +1,49 @@
 package io.github.thuliosouza.scholar_rest_service.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import io.github.thuliosouza.scholar_rest_service.domain.teacher.Teacher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Teacher teacher) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withIssuer("scholar-rest-service")
+                .withSubject(teacher.getEmail())
+                .withExpiresAt(expiresAt())
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("scholar-rest-service")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            return null;
+        }
+    }
+
+    private Instant expiresAt() {
+        return LocalDateTime.now()
+                .plusHours(8)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
 }
