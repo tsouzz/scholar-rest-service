52127ae4325edb29f80448381f655735c76f8192
thulio <thulioalves927@gmail.com>
2026-08-22 22:16:12 -0300

    refactor: altera o retorno de findSchoolByName para Optional

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/school/SchoolRepository.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/school/SchoolRepository.java
@@ -3,11 +3,12 @@ package io.github.thuliosouza.scholar_rest_service.domain.school;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SchoolRepository extends JpaRepository<School, UUID> {
 
-    public School findSchoolByName(String name);
+    public Optional<School> findSchoolByName(String name);
 
 }
