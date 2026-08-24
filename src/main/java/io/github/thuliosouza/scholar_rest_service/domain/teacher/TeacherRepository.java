fa7b96be8d0ea0d949d678718155e0a75f02f8f6
thulio <thulioalves927@gmail.com>
2026-08-24 10:04:58 -0300

    refactor: altera o retorno de findByEmail para Optional

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/TeacherRepository.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/TeacherRepository.java
@@ -2,12 +2,12 @@ package io.github.thuliosouza.scholar_rest_service.domain.teacher;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
 
-    public Teacher findByEmail(String email);
-
+    public Optional<Teacher> findByEmail(String email);
 
 
 }
