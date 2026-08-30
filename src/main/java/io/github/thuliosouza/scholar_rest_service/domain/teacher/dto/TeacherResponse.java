d41e44cdb66e6a734e6771e867227a0e47e10229
thulio <thulioalves927@gmail.com>
2026-08-29 23:21:53 -0300

    refactor(teacher): adiciona factory method no TeacherResponse

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/dto/TeacherResponse.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/dto/TeacherResponse.java
@@ -1,10 +1,21 @@
 package io.github.thuliosouza.scholar_rest_service.domain.teacher.dto;
 
+import io.github.thuliosouza.scholar_rest_service.domain.school.School;
+import io.github.thuliosouza.scholar_rest_service.domain.teacher.Teacher;
+
+import java.util.Optional;
 import java.util.UUID;
 
-public record TeacherResponse(
-        UUID id,
-        String name,
-        String email,
-        String schoolName
-) {}
+public record TeacherResponse(UUID id, String name, String email, String schoolName) {
+
+    public static TeacherResponse from(Teacher teacher) {
+        return new TeacherResponse(
+                teacher.getId(),
+                teacher.getName(),
+                teacher.getEmail(),
+                Optional.ofNullable(teacher.getSchool())
+                        .map(School::getName)
+                        .orElse(null)
+        );
+    }
+}
