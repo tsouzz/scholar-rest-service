9c575858d5957a8606a07ec91488d9152a659e8e
thulio <thulioalves927@gmail.com>
2026-08-31 21:57:15 -0300

    refactor(classgroup): adiciona number e teacherId a ClassGroupRequest

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/classgroup/dto/ClassGroupRequest.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/classgroup/dto/ClassGroupRequest.java
@@ -2,9 +2,17 @@ package io.github.thuliosouza.scholar_rest_service.domain.classgroup.dto;
 
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 public record ClassGroupRequest(
 
+        @NotNull(message = "O número/nome da turma é obrigatório")
+        int number,
+
         @NotNull(message = "O módulo é obrigatório")
-        Module module
+        Module module,
+
+        @NotNull(message = "O ID do professor é obrigatório")
+        UUID teacherId
 
 ) {}
