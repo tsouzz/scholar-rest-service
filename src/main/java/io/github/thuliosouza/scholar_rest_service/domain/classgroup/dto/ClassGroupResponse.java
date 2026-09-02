117b34c4994bb77f94bbe28aa8cba91260892f04
thulio <thulioalves927@gmail.com>
2026-09-01 22:47:13 -0300

    refactor(classgroup): adiciona factory method a ClassGroupResponse

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/classgroup/dto/ClassGroupResponse.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/classgroup/dto/ClassGroupResponse.java
@@ -1,10 +1,26 @@
 package io.github.thuliosouza.scholar_rest_service.domain.classgroup.dto;
 
+import io.github.thuliosouza.scholar_rest_service.domain.classgroup.ClassGroup;
+import io.github.thuliosouza.scholar_rest_service.domain.teacher.Teacher;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public record ClassGroupResponse(
         UUID id,
-        String name,
+        int number,
         Module module,
         String teacherName
-) {}
+) {
+
+    public static ClassGroupResponse from(ClassGroup classGroup) {
+        return new ClassGroupResponse(
+                classGroup.getId(),
+                classGroup.getNumber(),
+                classGroup.getModule(),
+                Optional.ofNullable(classGroup.getTeacher())
+                        .map(Teacher::getName)
+                        .orElse(null)
+        );
+    }
+}
