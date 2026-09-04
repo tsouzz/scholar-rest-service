47f927135688263ffb6e506973d9629470bec991
thulio <thulioalves927@gmail.com>
2026-09-03 21:34:27 -0300

    refactor(teacher): altera retorno do método update para TeacherResponse

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/TeacherService.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/teacher/TeacherService.java
@@ -2,11 +2,14 @@ package io.github.thuliosouza.scholar_rest_service.domain.teacher;
 
 import io.github.thuliosouza.scholar_rest_service.domain.school.School;
 import io.github.thuliosouza.scholar_rest_service.domain.school.SchoolRepository;
+import io.github.thuliosouza.scholar_rest_service.domain.teacher.dto.TeacherRequest;
+import io.github.thuliosouza.scholar_rest_service.domain.teacher.dto.TeacherResponse;
 import io.github.thuliosouza.scholar_rest_service.domain.teacher.exception.TeacherNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.UUID;
 
 @Service
@@ -17,63 +20,43 @@ public class TeacherService {
     private final SchoolRepository schoolRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public Teacher createTeacher(
-            String name,
-            String email,
-            String rawPassword,
-            String schoolName
-    ) {
-        School school = schoolRepository.findSchoolByName(schoolName)
-                .orElseGet(() -> schoolRepository.save(
-                        School.builder()
-                                .name(schoolName)
-                                .build()
-                ));
-
-        String passwordHash = passwordEncoder.encode(rawPassword);
-
-        Teacher teacher = Teacher.builder()
-                .name(name)
-                .email(email)
-                .passwordHash(passwordHash)
-                .school(school)
-                .build();
-
-        return teacherRepository.save(teacher);
+    public TeacherResponse findById(UUID teacherId) {
+        return TeacherResponse.from(getTeacherEntity(teacherId));
     }
 
     @Transactional
-    public Teacher updateTeacher(
-            UUID id,
-            String name,
-            String email,
-            String rawPassword
-    ){
-        Teacher teacher = findById(id);
+    public TeacherResponse update(UUID teacherId, TeacherRequest request) {
+        Teacher teacher = getTeacherEntity(teacherId);
 
-        teacher.setName(name);
-        teacher.setEmail(email);
+        teacher.setName(request.name());
+        teacher.setEmail(request.email());
 
-        if (rawPassword != null && !rawPassword.isBlank()) {
-            teacher.setPasswordHash(passwordEncoder.encode(rawPassword));
+        if (request.password() != null && !request.password().isBlank()) {
+            teacher.setPasswordHash(passwordEncoder.encode(request.password()));
         }
 
-        return teacher;
+        if (request.schoolName() != null && !request.schoolName().isBlank()) {
+            School school = schoolRepository.findSchoolByName(request.schoolName())
+                    .orElseGet(() -> schoolRepository.save(
+                            School.builder()
+                                    .name(request.schoolName())
+                                    .build()
+                    ));
+            teacher.setSchool(school);
+        }
+
+        return TeacherResponse.from(teacherRepository.save(teacher));
     }
 
     @Transactional
-    public void deleteTeacher(UUID teacherId){
-        teacherRepository.deleteById(teacherId);
+    public void delete(UUID teacherId) {
+        teacherRepository.delete(getTeacherEntity(teacherId));
     }
 
-    public Teacher findById(UUID teacherId){
+    private Teacher getTeacherEntity(UUID teacherId) {
         return teacherRepository.findById(teacherId)
-                .orElseThrow(() -> new TeacherNotFoundException(String.format("Professor com id %s não encontrado", teacherId)));
-    }
-
-    public Teacher findByEmail(String email){
-        return teacherRepository.findByEmail(email)
-                .orElseThrow();
+                .orElseThrow(() -> new TeacherNotFoundException(
+                        String.format("Professor com id %s não encontrado.", teacherId)
+                ));
     }
 }
