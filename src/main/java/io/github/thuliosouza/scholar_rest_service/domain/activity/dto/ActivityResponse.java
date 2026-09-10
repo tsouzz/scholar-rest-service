5a538999a74b19c221d19b323951e2c1ef8d919b
thulio <thulioalves927@gmail.com>
2026-09-09 22:14:09 -0300

    feat: adiciona factory method 'from' em ActivityResponse

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/activity/dto/ActivityResponse.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/activity/dto/ActivityResponse.java
@@ -1,14 +1,25 @@
 package io.github.thuliosouza.scholar_rest_service.domain.activity.dto;
 
+import io.github.thuliosouza.scholar_rest_service.domain.activity.Activity;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record ActivityResponse(
         UUID id,
-        String activityLabel,
         BigDecimal grade,
         LocalDate registrationDate,
         UUID studentId,
         String studentName
-) {}
+) {
+    public static ActivityResponse from(Activity activity) {
+        return new ActivityResponse(
+                activity.getId(),
+                activity.getGrade(),
+                activity.getRegistrationDate(),
+                activity.getStudent().getId(),
+                activity.getStudent().getName()
+        );
+    }
+}
