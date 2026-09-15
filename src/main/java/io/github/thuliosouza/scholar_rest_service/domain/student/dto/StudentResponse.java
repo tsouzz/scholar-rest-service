8b6a8db364a633a13f8e4fa6528cc7e7e114d30d
thulio <thulioalves927@gmail.com>
2026-09-14 23:53:59 -0300

    refactor: adiciona factory method from a StudentResponse

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/student/dto/StudentResponse.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/student/dto/StudentResponse.java
@@ -1,12 +1,21 @@
 package io.github.thuliosouza.scholar_rest_service.domain.student.dto;
 
-import java.math.BigDecimal;
+import io.github.thuliosouza.scholar_rest_service.domain.student.Student;
+
 import java.util.UUID;
 
 public record StudentResponse(
         UUID id,
         String name,
         UUID ClassGroupId,
-        String ClassGroupName,
-        BigDecimal grade
-) {}
+        String ClassGroupName
+) {
+    public static StudentResponse from(Student student) {
+        return new StudentResponse(
+                student.getId(),
+                student.getName(),
+                student.getClassGroup().getId(),
+                student.getClassGroup().getName()
+        );
+    }
+}
