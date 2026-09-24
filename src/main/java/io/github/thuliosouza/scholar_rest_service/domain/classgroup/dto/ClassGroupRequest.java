cd3524652f6c0f23611ae5099b0b146db4242acf
thulio <thulioalves927@gmail.com>
2026-09-23 21:57:24 -0300

    refactor: remove number e teacherId do ClassGroupRequest

--- src/main/java/io/github/thuliosouza/scholar_rest_service/domain/classgroup/dto/ClassGroupRequest.java
+++ src/main/java/io/github/thuliosouza/scholar_rest_service/domain/classgroup/dto/ClassGroupRequest.java
@@ -7,13 +7,7 @@ import java.util.UUID;
 
 public record ClassGroupRequest(
 
-        @NotNull(message = "O número/nome da turma é obrigatório")
-        int number,
-
         @NotNull(message = "O módulo é obrigatório")
-        Module module,
-
-        @NotNull(message = "O ID do professor é obrigatório")
-        UUID teacherId
+        Module module
 
 ) {}
